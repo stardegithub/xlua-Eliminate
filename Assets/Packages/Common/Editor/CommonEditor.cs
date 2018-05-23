f78762fe2bac74632bebbaa20da6a48dcfb87139
何利强 <heliqiang@jjhgame.com>
2018-05-23 14:59:32 +0800

    perf(追加UI_Base创建功能): 为了统一制造预制体，提供创建UI_Base功能。具体步骤：鼠标右键 -> Create -> UI -> Base

--- Assets/Packages/Common/Editor/CommonEditor.cs
+++ Assets/Packages/Common/Editor/CommonEditor.cs
@@ -9,7 +9,36 @@ namespace Common {
 	/// </summary>
 	public class CommonEditor : Editor {
 
-		#region UI
+#region UI
+
+		/// <summary>
+		/// 创建下载用Bar.
+		/// </summary>
+		[MenuItem("GameObject/UI/Base")]	
+		static void CreateUIBase () {	
+			GameObject selectedObj = Selection.activeGameObject;
+			if (selectedObj == null) {
+				return;
+			}
+			GameObject progressBar = GameObject.Instantiate (Resources.Load<GameObject> ("Prefab/UI_Base"));
+			progressBar.transform.parent = selectedObj.transform;
+			progressBar.transform.localScale = new Vector3 (1.0f, 1.0f, 1.0f);
+
+			RectTransform rectTrans = progressBar.transform as RectTransform;
+			if (rectTrans != null) {
+				rectTrans.pivot = new Vector2 (0.5f, 0.5f);
+				// 上边距 5
+				rectTrans.offsetMax = new Vector2 (rectTrans.offsetMax.x, -5);
+				// 右边距 5
+				rectTrans.offsetMax = new Vector2 (-5, rectTrans.offsetMax.y);
+
+				// 下边距 5
+				rectTrans.offsetMin = new Vector2 (rectTrans.offsetMin.x, 5);
+				// 左边距 5
+				rectTrans.offsetMin = new Vector2 (5, rectTrans.offsetMin.y);
+			}
+
+		}
 
 		/// <summary>
 		/// 创建下载用Bar.
@@ -40,6 +69,6 @@ namespace Common {
 
 		}
 
-		#endregion
+#endregion
 	}
 }
---
+++ Assets/Scripts/System/Const.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EC.System {
+
+	/// <summary>
+	/// 静态常量定义（全局）.
+	/// </summary>
+	public static class Const {
+
+		/// <summary>
+		/// 预制体根目录.
+		/// </summary>
+		public static readonly string PREFAB_ROOT_DIR = "Assets/Resources/Prefab";
+	}
+}
